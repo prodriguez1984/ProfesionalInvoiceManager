c0311dd62d45896e117f8b6775090e23aeb98b1e
Mati Sandler <matias44@gmail.com>
2019-10-06 22:27:44 -0300

    Modificacion Industry, Creación TaxInformation y CLient
    Se volvió para atrás el aspecto de la app y se migró a fragment la view Industry. Se llevaron los campos de las taxInformation a un nuevo fragment para poder reutilizarlo.

--- app/src/main/java/com/ort/profesionalinvoicemanager/views/Industry.java
+++ app/src/main/java/com/ort/profesionalinvoicemanager/views/Industry.java
@@ -5,19 +5,25 @@ import android.os.Bundle;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
 
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 
-public class Industry extends AppCompatActivity {
+public class Industry extends Fragment {
 
-    @Override
+    /*@Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_industry);
         Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+        //setSupportActionBar(toolbar);
 
         FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -27,6 +33,13 @@ public class Industry extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
-    }
+    }*/
 
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        //return super.onCreateView(inflater, container, savedInstanceState);
+
+        return inflater.inflate(R.layout.content_industry, container, false);
+    }
 }
