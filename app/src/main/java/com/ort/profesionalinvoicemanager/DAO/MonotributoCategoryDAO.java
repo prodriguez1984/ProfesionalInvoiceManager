5f34fa5468e1a776243f210ed05e7e11bc5bb222
prodriguez1984 <pablorodri1984@gmail.com>
2019-11-16 19:40:40 -0300

    Modelo Persistencia

--- app/src/main/java/com/ort/profesionalinvoicemanager/DAO/MonotributoCategoryDAO.java
+++ app/src/main/java/com/ort/profesionalinvoicemanager/DAO/MonotributoCategoryDAO.java
@@ -3,6 +3,7 @@ package com.ort.profesionalinvoicemanager.DAO;
 import android.database.Cursor;
 
 import com.ort.profesionalinvoicemanager.model.base.AbstractDao;
+import com.ort.profesionalinvoicemanager.model.base.PersistentObject;
 import com.ort.profesionalinvoicemanager.model.tax.MonotributoCategory;
 
 import java.util.ArrayList;
@@ -22,22 +23,16 @@ public class MonotributoCategoryDAO extends AbstractDao {
         return instance;
     }
 
-    public ArrayList<MonotributoCategory> getAll() {
-        ArrayList<MonotributoCategory> lstDocs = new ArrayList<>();
-        Cursor c = executeSqlQuery("Select * FROM " + TABLE, null);
-        if (c.getCount() == 0) {
-            return null;
-        }
-        if (c.moveToFirst()) {
-            while (!c.isAfterLast()) {
-                MonotributoCategory monotributoCategory = new MonotributoCategory();
-                monotributoCategory.setOid(c.getString(c.getColumnIndex(MonotributoCategory.KEY_OID)));
-                monotributoCategory.setCategory(c.getString(c.getColumnIndex(MonotributoCategory.KEY_CATEGORY)));
-                lstDocs.add(monotributoCategory);
-                c.moveToNext();
-            }
-        }
+    @Override
+    protected String getTableNameForModel() {
+        return MonotributoCategory.TABLE;
+    }
 
-        return lstDocs;
+    @Override
+    protected MonotributoCategory mapFromCursor(Cursor c) {
+        MonotributoCategory monotributoCategory = new MonotributoCategory();
+        monotributoCategory.setOid(c.getString(c.getColumnIndex(MonotributoCategory.KEY_OID)));
+        monotributoCategory.setCategory(c.getString(c.getColumnIndex(MonotributoCategory.KEY_CATEGORY)));
+        return monotributoCategory;
     }
 }
