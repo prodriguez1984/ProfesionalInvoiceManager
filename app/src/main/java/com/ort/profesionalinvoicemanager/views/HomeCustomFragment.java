3f46145bcfa993afd08ed632cd7f6640fe1317a8
pablo <pablopodgaiz@gmail.com>
2019-11-23 17:22:29 -0300

    agrgeo home

--- app/src/main/java/com/ort/profesionalinvoicemanager/views/HomeCustomFragment.java
+++ app/src/main/java/com/ort/profesionalinvoicemanager/views/HomeCustomFragment.java
@@ -5,10 +5,15 @@ import android.net.Uri;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+
+import com.ort.profesionalinvoicemanager.views.ui.ClientList.ClientListFragment;
 
 
 /**
@@ -28,6 +33,10 @@ public class HomeCustomFragment extends Fragment {
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
+
+    private Button btnCreateClient;
+    private Button btnCreateProduct;
+    private Button btnBill;
 
     private OnFragmentInteractionListener mListener;
 
@@ -66,8 +75,30 @@ public class HomeCustomFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home_custom, container, false);
+//        return inflater.inflate(R.layout.fragment_home_custom, container, false);
+//        configView();
+        container.removeAllViews();
+        View view = inflater.inflate(R.layout.fragment_home_custom, container, false);
+        configView(view);
+        return view;
     }
+
+    private void configView(View view) {
+        btnCreateClient = (Button)view.findViewById(R.id.btnCreateClient);
+        btnCreateClient.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ClientFragment nextFrag= new ClientFragment();
+                Fragment clientListFragment = new ClientListFragment();
+                getActivity().getSupportFragmentManager().beginTransaction()
+                        .replace(R.id.nav_host_fragment, clientListFragment)
+                        .commit();
+            }
+        });
+        btnCreateProduct = (Button)view.findViewById(R.id.btnCreateProduct);
+        btnBill = (Button)view.findViewById(R.id.btnBill);
+    }
+
 
     // TODO: Rename method, update argument and hook method into UI event
     public void onButtonPressed(Uri uri) {
@@ -76,22 +107,22 @@ public class HomeCustomFragment extends Fragment {
         }
     }
 
-    @Override
-    public void onAttach(Context context) {
-        super.onAttach(context);
-        if (context instanceof OnFragmentInteractionListener) {
-            mListener = (OnFragmentInteractionListener) context;
-        } else {
-            throw new RuntimeException(context.toString()
-                    + " must implement OnFragmentInteractionListener");
-        }
-    }
-
-    @Override
-    public void onDetach() {
-        super.onDetach();
-        mListener = null;
-    }
+//    @Override
+//    public void onAttach(Context context) {
+//        super.onAttach(context);
+//        if (context instanceof OnFragmentInteractionListener) {
+//            mListener = (OnFragmentInteractionListener) context;
+//        } else {
+//            throw new RuntimeException(context.toString()
+//                    + " must implement OnFragmentInteractionListener");
+//        }
+//    }
+//
+//    @Override
+//    public void onDetach() {
+//        super.onDetach();
+//        mListener = null;
+//    }
 
     /**
      * This interface must be implemented by activities that contain this
